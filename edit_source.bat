91bbade10e7884080229c4125e22a4b86f2e5c7a
Thijs Schreijer <thijs@thijsschreijer.nl>
2011-10-21 13:08:12 +0200

    fixed .bat file

--- edit_source.bat
+++ edit_source.bat
@@ -1,6 +1,7 @@
 c:
 cd %LUA_SOURCEPATH%
-start ..\scite\scite.exe xpl*.lua
+start ..\scite\scite.exe xpllogger.lua
+start ..\scite\scite.exe xplsender.lua
 start ..\scite\scite.exe xpl\*.lua
 start ..\scite\scite.exe xpl\classes\*.lua
 
