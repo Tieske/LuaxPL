dd0f67ed8d9e91bf1ee29d4dd169c6ea4f1be861
Thijs Schreijer <thijs@thijsschreijer.nl>
2011-11-24 20:59:11 +0100

    Updated batch files

--- 1_edit_source.bat
+++ 1_edit_source.bat
@@ -1,10 +1,25 @@
-c:
-cd %LUA_SOURCEPATH%
-start ..\scite\scite.exe xpllogger.lua
-start ..\scite\scite.exe xplsender.lua
-start ..\scite\scite.exe xplrun.lua
-start ..\scite\scite.exe xplrun_config.lua
-start ..\scite\scite.exe xpl\*.lua
-start ..\scite\scite.exe xpl\classes\*.lua
+@echo off
+echo off
+cls
+rem =================================================================
+rem The following variables should be defined;
+rem   LUA_DEV         - path to the Lua environment
+rem                     usually; c:\program files\lua\5.1
+rem   LUA_EDITOR      - path including executable of lua editor
+rem                     usually; %LUA_DEV%\scite\scite.exe
+rem   LUA_SOURCEPATH  - path to Lua source code
+rem                     usually; %LUA_DEV%\lua
+rem =================================================================
+rem
+rem USE: batch file opens all related files in the editor.
+rem      test files from their original location, main files
+rem      from the lua sourcepath location.
 
+rem Open all files in editor
+start "Lua" "%LUA_EDITOR%" "%LUA_SOURCEPATH%\xpllogger.lua"
+start "Lua" "%LUA_EDITOR%" "%LUA_SOURCEPATH%\xplsender.lua"
+start "Lua" "%LUA_EDITOR%" "%LUA_SOURCEPATH%\xplrun.lua"
+start "Lua" "%LUA_EDITOR%" "%LUA_SOURCEPATH%\xplrun_config.lua"
+start "Lua" "%LUA_EDITOR%" "%LUA_SOURCEPATH%\xpl\*.lua"
+start "Lua" "%LUA_EDITOR%" "%LUA_SOURCEPATH%\xpl\classes\*.lua"
 
